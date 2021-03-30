e89d0be2abee4d3a1501dfdbd8bba69cb4084ddb
Đinh Quốc Cường <quoccuong137@gmail.com>
2021-03-30 17:30:54 +0700

    Update DigitalVideoDisc.java

--- week5/AimsProject/src/Hust/Soict/Hedspi/Aim/Disc/DigitalVideoDisc.java
+++ week5/AimsProject/src/Hust/Soict/Hedspi/Aim/Disc/DigitalVideoDisc.java
@@ -1,18 +1,18 @@
 package Disc;
 
-
 public class DigitalVideoDisc {
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
-        this.title=title;
+        this.title = title;
     }
 
     public String getCategory() {
@@ -45,7 +45,7 @@ public class DigitalVideoDisc {
         this.cost = cost;
     }
 
-    public DigitalVideoDisc(float cost,String category,String title ){
+    public DigitalVideoDisc(float cost, String category, String title) {
 
     }
 
@@ -64,19 +64,26 @@ public class DigitalVideoDisc {
         this.cost = cost;
     }
 
-    public boolean search(String title){
-        int count=0;
+    public boolean search(String title) {
+        int count = 0;
         String[] s1 = getTitle().split("\\s");
         String[] s2 = title.split("\\s");
-        for(String s3 : s2){
-            for(String s4 : s1){
-                if(s4.equalsIgnoreCase(s3)){
+        for (String s3 : s2) {
+            for (String s4 : s1) {
+                if (s4.equalsIgnoreCase(s3)) {
                     count++;
                 }
             }
         }
-        if(count==s2.length) return true;
-        else return false;
+        if (count == s2.length)
+            return true;
+        else
+            return false;
     }
-    
+
+    public String showInfo() {
+        return "DVD: Title: " + getTitle() + " Director: " + getDirector() + " Category: " + getCategory() + " Price: "
+                + getCost();
+    }
+
 }
